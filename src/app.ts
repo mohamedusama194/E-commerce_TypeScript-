9086fae0625d0349702f5a7e2b6dcb8c8ac1080b
Mohamed Usama <mohamedusama421@gmail.com>
2026-01-09 20:35:09 +0200

    feat: enhance application structure with authentication and category routes, integrate PostgreSQL database, and add middleware for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import express from "express";
+import morgan from "morgan";
 import userRoutes from "./modules/user/user.route";
+import authRoutes from "./modules/auth/auth.route";
+import categoryRoutes from "./modules/category/category.route";
+import notFound from "./middlewares/notFound.middlware";
 import globalError from "./middlewares/error.middleware";
 
 const app = express();
 
+/* ================= Middlewares ================= */
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+    console.log("mode is :development");
+  app.use(morgan("dev"));
+}
 
 /* ================= Routes ================= */
 app.use("/api/users", userRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
 /* ================= Error Handling ================= */
+app.use(notFound);
 app.use(globalError);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import "./env";
-import express from "express";
+import app from "./app";
+import { AppDataSource } from "./config/database";
 
-const app = express();
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const PORT = process.env.PORT || 8000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected ✅");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed ❌", err);
+  });
